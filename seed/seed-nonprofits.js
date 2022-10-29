de6c4107b3ef0123c1837abf0ff818a2d3772604
TannerEsch <eschmanntanner@gmail.com>
2022-10-29 13:15:23 -0400

    add: places & nonprofit seed

---
+++ seed/seed-nonprofits.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+// connect to datbase
+const MONGO_CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING;
+mongoose.connect(MONGO_CONNECTION_STRING, { useNewUrlParser: true });
+const db = mongoose.connection;
+const axios = require('axios')
+const apiKey = process.env.API_KEY;
+
+
+db.once('open', () => {
+    console.log(`Connected to MongoDB at HOST: ${db.host} and PORT: ${db.port}`);
+});
+
+db.on('error', (error) => {
+    console.log(`Database Error: ${error}`);
+});
+
+const Nonprofit = require('../models/nonprofit');
+
+function nonProfits() {
+    const array = [];
+    axios.get(`https://partners.every.org/v0.2/browse/climate?apiKey=${apiKey}`)
+    .then(response => {
+        array.push(response.data))
+        .then(array => {
+            Nonprofit.insertMany(array)
+        })
+            .catch(error => {
+        console.log(error);
+    })
+})
+.catch(error => {
+    console.log('ERROR', error);
+});
+}
+
+nonProfits();
+
+
+
